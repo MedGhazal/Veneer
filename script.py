74be33b43324f11919bda86076650208e10284e1
MedGhazal <ghazal@rhrk.uni-kl.de>
2020-03-28 22:34:03 +0100

    Some corretions to the tests and modf to the class Owner

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ class Art:
     self.owner = owner
     
   def __repr__(self):
-    return "{}. \"{}\". {}, {}. The owner is {}".format(self.artist, self.title, self.year, self.medium, self.owner.name)
+    return "{}. \"{}\". {}, {}. The owner is {} located in {}".format(self.artist, self.title, self.year, self.medium, self.owner.name, self.owner.location)
 
 class Marketplace:
   def __init__(self, *listings):
@@ -21,7 +21,7 @@ class Marketplace:
       print(listing)
 
 class Client:
-  def __init__(self, name, location, is_museum):
+  def __init__(self, name, is_museum, location = "Private Collection"):
     self.name = name 
     self.location = location
     self.is_museum = is_museum 
@@ -29,8 +29,8 @@ class Client:
 veneer = Marketplace([])
 veneer.show_listing()
 
-adytta = Client("Edytta Hapirt", None, False)
-moma = Client("The MOMA", "New York", True)
+adytta = Client("Edytta Hapirt", False)
+moma = Client("The MOMA", True, "New York")
 
 girl_with_mandolin = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)", 1910, "oil on canvas", adytta)
 print(girl_with_mandolin)

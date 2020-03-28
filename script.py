17f80bed16ddbed96a3d42f985e527d597113398
MedGhazal <ghazal@rhrk.uni-kl.de>
2020-03-28 22:30:12 +0100

    Updated the class Art

--- script.py
+++ script.py
@@ -1,15 +1,16 @@
 class Art:
-  def __init__(self, artist, title, medium, year):
+  def __init__(self, artist, title, medium, year, owner):
     self.artist = artist 
     self.title = title 
     self.medium = medium 
     self.year = year
+    self.owner = owner
     
   def __repr__(self):
-    return "{}. \"{}\". {}, {}".format(self.artist, self.title, self.year, self.medium)
+    return "{}. \"{}\". {}, {}. The owner is {}".format(self.artist, self.title, self.year, self.medium, self.owner.name)
 
 class Marketplace:
-  def __init__(self, listings):
+  def __init__(self, *listings):
     self.listings = listings
   def add_listing(self, new_listing):
     self.listing.append(new_listing)
@@ -19,10 +20,17 @@ class Marketplace:
     for listing in self.listings:
       print(listing)
 
-girl_with_mandolin = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)", 1910, "oil on canvas")
+class Client:
+  def __init__(self, name, location, is_museum):
+    self.name = name 
+    self.location = location
+    self.is_museum = is_museum 
 
-print(girl_with_mandolin)
-
-veneer = Marketplace([girl_with_mandolin])
+veneer = Marketplace([])
 veneer.show_listing()
 
+adytta = Client("Edytta Hapirt", None, False)
+moma = Client("The MOMA", "New York", True)
+
+girl_with_mandolin = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)", 1910, "oil on canvas", adytta)
+print(girl_with_mandolin)
